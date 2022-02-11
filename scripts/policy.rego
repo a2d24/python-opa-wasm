fb48a89e4b458628d59be58d159cae6aa4615007
imtiazmangerah <57962793+imtiazmangerah@users.noreply.github.com>
2022-02-11 02:28:00 +0200

    fix: fixes memory leak during fastpath evaluation (#6)
    Closes #5
    
    When using fastpath evaluation (ABI >= 1.2) and making multiple calls to evaluate(...) without a
    call to set_data(...), the wasm modules memory usage and/or page size grows on each evaluation.

---
+++ scripts/policy.rego
@@ -0,0 +1,6 @@
+package sample
+
+test := {
+    "test_1": data.test_1,
+    "test_2": input.test_2,
+}
--- tests/test_memory.py
+++ tests/test_memory.py
@@ -27,3 +27,19 @@ def test_memory_corruption_type_1():
             'result': {'allow': True, 'unique_roles': ['AAAAAAAAAAAAAAA#BBBBB#CCCCC']}
         }
     ]
+
+
+def test_memory_leak():
+    # Regression test - fastpath memory leak
+    policy = OPAPolicy('./policy_memory_check.wasm', memory_pages=2, max_memory_pages=2)
+    policy.set_data({})
+    input = {
+        "roles": ["AAAAAAAAAAAAAAA#BBBBB#CCCCC" for _ in range(5)]
+    }
+
+    for _ in range(1000):
+        assert policy.evaluate(input) == [
+            {
+                'result': {'allow': True, 'unique_roles': ['AAAAAAAAAAAAAAA#BBBBB#CCCCC']}
+            }
+        ]
